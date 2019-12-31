85e34f00e47763d7739b593aaf09151d3991ef55
Joanna E K <kalemajoanna@gmail.com>
2019-12-31 21:44:48 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,30 @@
-class Person 
-  
-   def initialize(name)
-    @name = name
-  end
-  #   def name=(name)
-  #   @name = name
-  # end 
-  
-  # def name
-  #   @name
-  # end 
+class Person
 end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Person 
+  
+#   def initialize(name)
+#     @name = name
+#   end
+#   #   def name=(name)
+#   #   @name = name
+#   # end 
+  
+#   # def name
+#   #   @name
+#   # end 
+# end 
